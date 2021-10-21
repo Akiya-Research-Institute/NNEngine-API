99e100605bb621b5d2aab14b7e1a885f2f71d26e
Kenji Asaba <kasa@akiya-souken.co.jp>
2021-10-21 17:29:04 +0900

    構成これでええやろ

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Optimized Machine Learning Inference for Unreal", "index.html", [
+  [ "NNEngine", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
--- search/all_5.js
+++ search/all_5.js
@@ -7,5 +7,6 @@ var searchData=
   ['getinverseaffinemat_4',['getInverseAffineMat',['../class_u_texture_process_function_library.html#a9d8f02132f0e27963372ee18b8cd68a1',1,'UTextureProcessFunctionLibrary']]],
   ['getinverseaffinematfrom2points_5',['getInverseAffineMatFrom2Points',['../class_u_texture_process_function_library.html#a2ab5b0e53dcf82e3e4c222df0d5acd8f',1,'UTextureProcessFunctionLibrary']]],
   ['getoutputtensorsinfo_6',['getOutputTensorsInfo',['../class_u_onnx_model_wrapper.html#a9b824227fc1b174238030a869b1a6332',1,'UOnnxModelWrapper']]],
-  ['getpixeldata_7',['getPixelData',['../class_u_texture_process_component.html#ae47a677909c282f7327b5f8a59c0dc18',1,'UTextureProcessComponent']]]
+  ['getpixeldata_7',['getPixelData',['../class_u_texture_process_component.html#ae47a677909c282f7327b5f8a59c0dc18',1,'UTextureProcessComponent']]],
+  ['getpluginpath_8',['getPluginPath',['../class_u_onnx_utility_library.html#add54946bfe75c70d66a74a29e3025186',1,'UOnnxUtilityLibrary']]]
 ];
